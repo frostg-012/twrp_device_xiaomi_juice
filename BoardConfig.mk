5e408c4b0d454cef175f3eed7137cf2aac65fa3f
frost (frostg-012) <optimus7907@gmail.com>
2021-12-05 21:38:36 +0700

    juice: Add assert for citrus, lime, lemon, and pomelo
    Signed-off-by: frost (frostg-012) <optimus7907@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,3 +26,6 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := citrus,lime,lemon,pomelo
