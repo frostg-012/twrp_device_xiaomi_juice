0febe6ab161989cd35dda7d24b91e83a5b2f2775
frost (frostg-012) <optimus7907@gmail.com>
2021-12-05 22:09:06 +0700

    juice: Configure filesystem flags
    * Enable EXT4 & F2FS
    * Set BOARD_HAS_LARGE_FILESYSTEM to true
    
    Signed-off-by: frost (frostg-012) <optimus7907@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,11 @@ TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
+# Filesystem
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.init_fatal_reboot_target=recovery
